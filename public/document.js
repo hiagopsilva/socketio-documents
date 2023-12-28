253196eec07d0fabdcb868b0533d9cee0a722c18
Hiago Prates <hiagohps360@gmail.com>
2023-12-28 13:18:41 -0300

    feat: get value form client frontend

--- public/document.js
+++ public/document.js
@@ -1,1 +1,7 @@
 const socket = io()
+
+const textEditor = document.getElementById('editor-texto')
+
+textEditor.addEventListener('keyup', () => {
+  socket.emit('text_editor', textEditor.value)
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,9 @@ import io from './server.js'
 
 io.on('connection', socket => {
   console.log(`Socket conectado: ${socket.id}`)
+
+  socket.on('text_editor', text => {
+    socket.broadcast.emit('text_editor', text)
+    console.log(`Texto: ${text}`)
+  })
 })
