8659198289bb1cf65158feb7c3219f8dee8ca0fb
Hiago Prates <hiagohps360@gmail.com>
2023-12-28 13:22:59 -0300

    feat: send data for broadcast

--- public/document.js
+++ public/document.js
@@ -5,3 +5,8 @@ const textEditor = document.getElementById('editor-texto')
 textEditor.addEventListener('keyup', () => {
   socket.emit('text_editor', textEditor.value)
 })
+
+socket.on('text_editor_clients', text => {
+  console.log({text})
+  textEditor.value = text
+})
--- src/socket-back.js
+++ src/socket-back.js
@@ -4,7 +4,7 @@ io.on('connection', socket => {
   console.log(`Socket conectado: ${socket.id}`)
 
   socket.on('text_editor', text => {
-    socket.broadcast.emit('text_editor', text)
+    socket.broadcast.emit('text_editor_clients', text)
     console.log(`Texto: ${text}`)
   })
 })
